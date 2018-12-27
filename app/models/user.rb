4edb623b73287eb482a8efbe7ce45dd1fd47d978
Daisuke Yamaguchi <freedomatbones@gmail.com>
2018-12-27 14:47:50 +0900

    doorkeeperの管理者制限
    $ bin/rails g migration AddOmniauthToUser uid name admin:boolean
    $ bin/rails db:migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+  after_create :update_uid!
+
+  def update_uid!
+    update(uid: id)
+  end
 end
