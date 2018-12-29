9695b7f5a2243ff1b017ce7b2b3a7135e9a229b9
Daisuke Yamaguchi <freedomatbones@gmail.com>
2018-12-29 23:27:09 +0900

    sessionにリダイレクト情報が存在する場合はそちらにリダイレクト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   use_doorkeeper_openid_connect
   use_doorkeeper
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => 'home#index'
